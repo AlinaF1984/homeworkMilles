a0e5a910a62ab5012772edc8e0bf3e5309610606
AlinaF <afassahova9@gmail.com>
2022-09-04 22:34:04 +0500

    add code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 100 ;
+        int milli = 20 ;
+        int bonus = ticketPrice /milli ;
+        System.out.println(ticketPrice /milli);
+
+    }
+}
